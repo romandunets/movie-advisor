599ea1f2f52bad2724bfe7bd05763e86dea00b93
Roman Dunets <roman.dunets.email@gmail.com>
2016-09-22 15:42:57 +0300

    Add users table in create db query

--- database/create_db.sql
+++ database/create_db.sql
@@ -9,3 +9,23 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `role_name_unique` (`name`)
 );
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `salt` VARCHAR(128) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `first_name` VARCHAR(50) DEFAULT NULL,
+  `second_name` VARCHAR(50) DEFAULT NULL,
+  `birthday` DATE DEFAULT NULL,
+  `gender` BOOLEAN DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `role_id` BIGINT(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_username_unique` (`username`),
+  UNIQUE KEY `user_email_unique` (`email`),
+  CONSTRAINT `fk_users_to_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+);
