265b401bf4b83b1b9e14a075cd641bb61097176b
Roman Dunets <roman.dunets.email@gmail.com>
2016-09-22 15:43:27 +0300

    Add follower_to_followed table in create db query

--- database/create_db.sql
+++ database/create_db.sql
@@ -29,3 +29,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `user_email_unique` (`email`),
   CONSTRAINT `fk_users_to_roles` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `follower_to_followed` (
+  `follower_id` BIGINT(20) NOT NULL,
+  `followed_id` BIGINT(20) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `fk_follower_to_followed_to_users` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_followed_to_followed_to_users` FOREIGN KEY (`followed_id`) REFERENCES `users` (`id`)
+);
