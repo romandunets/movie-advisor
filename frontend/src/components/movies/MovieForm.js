aae93244f046573c1d75f9b0fb08bcb791cb63fa
Roman Voglhuber <r.voglhuber@gmail.com>
2016-11-14 12:31:44 +0100

    Added feature to load data from OMDB when adding a new Movie

--- frontend/src/components/movies/MovieForm.js
+++ frontend/src/components/movies/MovieForm.js
@@ -3,47 +3,78 @@ import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
 import { compose } from 'redux';
+import axios from 'axios';
 
 class MovieForm extends Component {
+
+  //Load info from OMDB
+  searchOMDB(change){
+    var title = document.getElementsByName("title")[0].value;
+
+    if(title.length > 0){
+      axios("http://www.omdbapi.com/?t="+title+"&y=&plot=short&r=json")
+        .then(function(res){
+          console.log(res);
+          var data = res.data;
+          console.log(data);
+          if(res.data.Response != "False"){
+            change("title", data.Title);
+            change("description", data.Plot);
+            change("year", data.Year);
+            change("image", data.Poster);
+            change("producer", data.Director);
+            change("duration", data.Runtime.replace(" min", ""));
+            change("age_restriction", data.Rated);
+          }
+      });
+    }
+  }
+
+  handleClick(e){
+    this.searchOMDB();
+  }
+
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, change } = this.props;
+    console.log(this.props);
     return (
       <form onSubmit={ handleSubmit }>
-        <div>
+        <div className="form-group">
           <label htmlFor="title">Title</label>
-          <Field name="title" component="input" type="text" />
+          <Field name="title" component="input" type="text" className="form-control" id="TitleInput"/>
+          <a href='#' onClick={this.searchOMDB.bind(this, change)} className="btn btn-secondary">Load from OMDB</a>
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="year">Year</label>
-          <Field name="year" component="input" type="text" />
+          <Field name="year" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="description">Description</label>
-          <Field name="description" component="input" type="text" />
+          <Field name="description" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="image">Image URL</label>
-          <Field name="image" component="input" type="text" />
+          <Field name="image" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="studio">Studio</label>
-          <Field name="studio" component="input" type="text" />
+          <Field name="studio" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="producer">Producer</label>
-          <Field name="producer" component="input" type="text" />
+          <Field name="producer" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="duration">Duration (min)</label>
-          <Field name="duration" component="input" type="text" />
+          <Field name="duration" component="input" type="text" className="form-control"  />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="age_restriction">Age restriction</label>
-          <Field name="age_restriction" component="input" type="text" />
+          <Field name="age_restriction" component="input" type="text" className="form-control"  />
         </div>
         
-        <button type="submit">Submit</button>
-        <a href='#' onClick={browserHistory.goBack}>Cancel</a>
+        <a href='#' onClick={browserHistory.goBack} className="btn btn-secondary">Cancel</a>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
