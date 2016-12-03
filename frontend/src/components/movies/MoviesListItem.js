ea6e84f5349f04924488f4b058e9791ac6f36225
Roman Dunets <roman.dunets.email@gmail.com>
2016-12-03 20:43:45 +0200

    Fine-tune movie item component

--- frontend/src/components/movies/MoviesListItem.js
+++ frontend/src/components/movies/MoviesListItem.js
@@ -2,43 +2,41 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const MoviesListItem = ({ movie, isAuthenticated }) => (
-  <div className="movie-item">
-    <div className="row">
-      <div className="thumbnail col-md-2">
-        <img src={ movie.image } className="img-fluid img-rounded"/>
-      </div>
-      <div className="col-md-10">
-        <div className="row">
-          <div className="col-md-6">
-            <h4>
-              { movie.title }
-               <small className="text-muted">({ movie.year })</small>
-            </h4>
-          </div>
-          <div className="col-md-6">
-            <div className="rating text-md-right">
-              <span>★</span>
-              <span>★</span>
-              <span>★</span>
-              <span>★</span>
-              <span>☆</span>
-            </div>
-          </div>
+  <div className="list-item">
+    <div className="thumbnail col-md-2">
+      <Link to={`/movies/${movie.id}`}>
+        <img src={ movie.image } className="img-fluid"/>
+      </Link>
+    </div>
+    <div className="col-md-10">
+      <div className="row">
+        <div className="col-md-6">
+          <h4>
+            <Link to={`/movies/${movie.id}`}>{ movie.title } ({ movie.year })</Link>
+          </h4>
         </div>
-        <div className="row">
-          <div className="col-md-12">
-            { movie.age_restriction } |  { movie.duration } min | Action, Adventure, Sci-Fi
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-8 plot">
-            <p>{ movie.description }</p>
+        <div className="col-md-6">
+          <div className="rating text-md-right">
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>☆</span>
           </div>
         </div>
       </div>
       <div className="row">
+        <div className="col-md-12">
+          { movie.age_restriction } |  { movie.duration } min | Action, Adventure, Sci-Fi
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-8 plot">
+          <p>{ movie.description }</p>
+        </div>
+      </div>
+      <div className="row">
         <div className="text-md-right">
-          <Link to={`/movies/${movie.id}`} role="button" className="btn btn-secondary">Details</Link>
           { isAuthenticated &&
             <Link to={`/movies/${movie.id}/watched`} role="button" className="btn btn-secondary">Watched</Link>
           }
