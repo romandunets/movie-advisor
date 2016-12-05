e4d59c44b5dda9220415d985199dbfe5aab3e475
Roman Dunets <roman.dunets.email@gmail.com>
2016-12-05 19:32:10 +0300

    Add pagination to movies list

--- frontend/src/containers/movies/MoviesListPage.js
+++ frontend/src/containers/movies/MoviesListPage.js
@@ -1,14 +1,19 @@
 import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { Pagination } from 'react-bootstrap';
 
-import * as movieActions from '../../actions/movieActions'
-import MoviesList from '../../components/movies/MoviesList'
+import * as movieActions from '../../actions/movieActions';
+import MoviesList from '../../components/movies/MoviesList';
 
 class MoviesListPage extends Component {
   componentWillMount() {
     this.props.actions.listMovies();
+  }
+
+  handlePageSelect(page) {
+    console.log(page);
   }
 
   render() {
@@ -17,6 +22,7 @@ class MoviesListPage extends Component {
       <div>
         <div className="header">
           <h3>All movies</h3>
+          <Pagination prev next first last ellipsis boundaryLinks items={10} maxButtons={5} activePage={1} onSelect={this.handlePageSelect} />
           { isAuthenticated && isAdmin &&
             <div className="actions text-md-right">
               <Link to='/movies/new' role="button" className="btn btn-primary btn-sm">New movie</Link>
