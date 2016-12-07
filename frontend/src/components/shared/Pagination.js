928405f2f683137ddfbf6d4ec23050cbe57936a1
Roman Dunets <roman.dunets.email@gmail.com>
2016-12-07 18:42:25 +0200

    Fix pagination component

--- frontend/src/components/shared/Pagination.js
+++ frontend/src/components/shared/Pagination.js
@@ -3,21 +3,21 @@ import { Link } from 'react-router';
 
 class Pagination extends Component {
   getPagesCount(total, visible, page) {
-    if (total < visble) {
+    if (total < visible) {
       return total; 
     }
     else {
-      const currentSet = Math.floor(page / visible);
-      const sets = Math.floor(total / visible);
+      const currentSet = Math.ceil(page / visible);
+      const sets = Math.ceil(total / visible);
       return (currentSet == sets) ? total % visible : visible;
     }
   }
 
   render() {
     const { onPageSelect, total, page = 1, visible = 5 } = this.props;
-    console.log(total % visible > 0 ? total % visible : visible);
+    const pagesCount = this.getPagesCount(total, visible, page);
+    const shift = Math.floor(page / (visible + 1)) * visible + 1;
 
-    const pagesCount = this.getPagesCount(total, visible, page)// total < visible ? total : ((total - page) < (visible + 1) ? (total % visible > 0 ? total % visible : visible) : visible);
     return (
       <div className="text-center">
         <nav aria-label="Page navigation">
@@ -29,8 +29,8 @@ class Pagination extends Component {
               </a>
             </li>
             { [...Array(pagesCount).keys()].map(p =>
-              <li key={p} className={`page-item ${(page - p == 1) ? 'active' : ''}`}>
-                <Link className="page-link" to='' onClick={(e) => onPageSelect(e, p + 1)}>{ p + 1 }</Link>
+              <li key={p} className={`page-item ${(page == p + shift) ? 'active' : ''}`}>
+                <Link className="page-link" to='' onClick={(e) => onPageSelect(e, p + shift)}>{ p + shift }</Link>
               </li>
             ) }
             <li className={`page-item ${(total - page < visible + 1) ? 'disabled' : ''}`}>
