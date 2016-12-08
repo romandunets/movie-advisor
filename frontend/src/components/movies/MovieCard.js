cd130ec91b3c92773ffe83cffaa5813f47280be8
Roman Dunets <roman.dunets.email@gmail.com>
2016-12-08 22:32:22 +0200

    Refactor movie card component

--- frontend/src/components/movies/MovieCard.js
+++ frontend/src/components/movies/MovieCard.js
@@ -1,14 +1,60 @@
 import React from 'react';
-
-import MovieThumbnail from './MovieThumbnail';
-import MovieInfo from './MovieInfo';
-import MovieActions from './MovieActions';
+import { Link } from 'react-router';
 
 const MovieCard = ({ movie, deleteMovie, isAuthenticated, isAdmin }) => (
   <div className="row">
-    <MovieThumbnail movie={movie} />
-    <MovieInfo movie={movie} deleteMovie={deleteMovie} isAuthenticated={isAuthenticated} isAdmin={isAdmin} />
-    <MovieActions movie={movie} deleteMovie={deleteMovie} isAuthenticated={isAuthenticated} />
+    <div className="thumbnail-box col-md-3">
+      <img src={ movie.image } className="img-fluid"/>
+    </div>
+    <div className="col-md-9">
+      <div className="row">
+        <div className="col-md-6">
+          <h4>
+            { movie.title }
+            <span className="text-muted"> ({ movie.year })</span>
+          </h4>
+        </div>
+        <div className="col-md-6">
+          <div className="float-xs-right">
+            { isAuthenticated &&
+              <Link to={`/movies/${movie.id}/watched`} role="button" className="btn btn-sm btn-secondary action">Watched</Link>
+            }
+            { isAuthenticated && isAdmin &&
+              <Link to={`/movies/${movie.id}/edit`} role="button" className="btn btn-sm btn-primary action">Edit</Link>
+            }
+            { isAuthenticated && isAdmin &&
+              <Link to='#' role="button" className="btn btn-sm btn-danger action" onClick={(e) => deleteMovie(movie.id)}>Delete</Link>
+            }
+          </div>
+          { isAuthenticated &&
+            <div className="float-xs-right">
+              <span className="match">&#10084;</span>
+              <span className="rating">{ movie.match }%</span>
+            </div>
+          }
+          <div className="float-xs-right">
+            <span className="star">&#9733;</span>
+            <span className="rating">{ movie.rating }</span>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          { movie.age_restriction } | { movie.duration } min | { movie.genres && movie.genres.join(', ') }
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-8 plot">
+          <p>{ movie.description }</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12 properties">
+          <p><b>Director:</b> { movie.director }</p>
+          <p><b>Studio:</b> { movie.studio }</p>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
--- frontend/src/components/movies/MovieThumbnail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MovieThumbnail = ({ movie }) => (
-  <div className="thumbnail-box col-md-3">
-    <img src={ movie.image } className="img-fluid"/>
-  </div>
-)
-
-export default MovieThumbnail;
