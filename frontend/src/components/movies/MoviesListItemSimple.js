613fdb30f89c8a4f617aef307ae07b9212b9532e
Roman Voglhuber <r.voglhuber@gmail.com>
2016-12-28 21:06:56 +0100

    Added description and simple movielist to homepage

---
+++ frontend/src/components/movies/MoviesListItemSimple.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class MoviesListItemSimple extends Component {
+  render() {
+    const { movie, isAuthenticated } = this.props;
+
+    return (
+      <div className="list-item">
+        <div className="thumbnail-box col-md-2">
+          <Link to={`/movies/${movie.id}`}>
+            <img src={ movie.coverImage } className="img-fluid simple"/>
+          </Link>
+        </div>
+        <div className="col-md-10 simple-text">
+          <div className="row">
+            <div className="col-md-6">
+              <h4>
+                <Link to={`/movies/${movie.id}`}>{ movie.title }</Link>
+                <span className="text-muted"> ({ movie.year })</span>
+              </h4>
+            </div>
+            <div className="col-md-6">
+              <div className="float-xs-right">
+                <span className="star">&#9733;</span>
+                <span className="rating">{ movie.rating }</span>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-8 plot">
+              <p>{ movie.description }</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MoviesListItemSimple;
