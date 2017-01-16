0716cdb985d7a274e094183d859ba41a6b01faab
Roman Dunets <roman.dunets.email@gmail.com>
2017-01-16 21:25:49 +0200

    Refactor progressBar into separate component

---
+++ frontend/src/components/shared/ProgressBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProgressBar extends Component {
+  render() {
+    return (
+      <div className="center-content">
+        <h3 className="title">Loading...</h3>
+      </div>
+    );
+  }
+}
+
+export default ProgressBar;
--- frontend/src/containers/users/UsersListPage.js
+++ frontend/src/containers/users/UsersListPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
 
 import * as userActions from '../../actions/userActions';
+import ProgressBar from '../../components/shared/ProgressBar'
 import UsersList from '../../components/users/UsersList';
 import UsersActionBar from '../../components/users/UsersActionBar';
 
@@ -16,11 +17,7 @@ class UsersListPage extends Component {
     const { users, isLoading, isAuthenticated, isAdmin } = this.props;
 
     if (isLoading) {
-      return (
-        <div className="center-content">
-          <h3 className="title">Loading...</h3>
-        </div>
-      );
+      return <ProgressBar />;
     } else {
       return (
         <div>
