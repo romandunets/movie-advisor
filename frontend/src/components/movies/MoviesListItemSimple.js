53236880481761240e9147f7305972a36d2eedbf
Roman Dunets <roman.dunets.email@gmail.com>
2017-01-19 21:39:04 +0300

    Fine-tune top movies list

--- frontend/src/components/movies/MoviesListItemSimple.js
+++ frontend/src/components/movies/MoviesListItemSimple.js
@@ -7,30 +7,26 @@ class MoviesListItemSimple extends Component {
 
     return (
       <div className="list-item">
-        <div className="thumbnail-box col-md-2">
+        <div className="thumbnail-box">
           <Link to={`/movies/${movie.id}`}>
             <img src={ movie.coverImage } className="img-fluid simple"/>
           </Link>
         </div>
-        <div className="col-md-10 simple-text">
-          <div className="row">
-            <div className="col-md-6">
-              <h4>
-                <Link to={`/movies/${movie.id}`}>{ movie.title }</Link>
-                <span className="text-muted"> ({ movie.year })</span>
-              </h4>
-            </div>
-            <div className="col-md-6">
-              <div className="float-xs-right">
-                <span className="star">&#9733;</span>
-                <span className="rating">{ movie.rating }</span>
-              </div>
+        <div className="content-box">
+          <div className="col-md-6">
+            <h4>
+              <Link to={`/movies/${movie.id}`}>{ movie.title }</Link>
+              <span className="text-muted"> ({ movie.year })</span>
+            </h4>
+          </div>
+          <div className="col-md-6">
+            <div className="float-xs-right">
+              <span className="star">&#9733;</span>
+              <span className="rating">{ movie.rating }</span>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-8 plot">
-              <p>{ movie.description }</p>
-            </div>
+          <div className="col-md-8 plot">
+            <p>{ movie.description }</p>
           </div>
         </div>
       </div>
