65a56ff08237e992c079b36ee6d3a3601a9c94a7
Roman Dunets <roman.dunets.email@gmail.com>
2017-02-10 23:21:47 +0300

    Change adding new notification with replacing all

--- frontend/src/reducers/notifications.js
+++ frontend/src/reducers/notifications.js
@@ -4,9 +4,9 @@ import initialState from './initialState';
 const notificationsReducer = (state = initialState.notifications, action) => {
   switch(action.type) {
     case types.SEND_NOTIFICATION_INFO:
-      return {...state, notifications: appendNotification(state, action, 'INFO')}
+      return {...state, notifications: createNotification(state, action, 'INFO')}
     case types.SEND_NOTIFICATION_ERROR:
-      return {...state, notifications: appendNotification(state, action, 'ERROR')}
+      return {...state, notifications: createNotification(state, action, 'ERROR')}
     case types.CLOSE_NOTIFICATION:
       return {...state, notifications: removeNotification(state, action)}
     case types.CLEAR_NOTIFICATIONS:
@@ -16,11 +16,15 @@ const notificationsReducer = (state = initialState.notifications, action) => {
   }
 }
 
-function appendNotification(state, action, type) {
+function createNotification(state, action, type) {
   const id = new Date().getTime()
-  const notification = { id, message: action.payload.message, type };
+  return [{ id, message: action.payload.message, type }];
+}
+
+function appendNotification(state, action, type) {
+  const notification = createNotification(state, action, type);
   if (!containsNotification(state, notification)) {
-    return state.notifications.concat([notification]);
+    return state.notifications.concat(notification);
   }
 }
 
